32d4b1c68a8cbcf2b86a3dd55e4a4bf53a8ad124
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-02-27 14:23:41 -0300

    add routes al componente app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
+import React from 'react';
+
 import './App.css';
-import { Body } from './components/main/body/Body';
-import { Header } from './components/main/header/Header';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-
-      </Header>
-      <Body>
-      
-      </Body>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
