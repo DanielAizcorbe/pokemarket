3bebabd92d394d350f059a557c4df3a1d87b898d
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-02-27 13:23:54 -0300

    add contenedor de columnas

--- src/utils/Flex.js
+++ src/utils/Flex.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./flex.css"
 
-const Rows = ({children, centered, justify, align, width, height, padding}) => {
+const Rows = ({ children, centered, justify, align, width, height, padding }) => {
 
     const classes = "rows " + (centered ? "centered" : "")
 
@@ -19,8 +19,31 @@ const Rows = ({children, centered, justify, align, width, height, padding}) => {
             style={rowStyles}
         >
             {children}
-        </div>  
+        </div>
     )
 }
 
-export { Rows }
+const Columns = ({ children, centered, justify, align, width, height, padding }) => {
+
+    const classes = "columns " + (centered ? "centered" : "")
+
+    const columnsStyles = {
+        justifyContent: justify || "",
+        alignItems: align || "",
+        width: width || "auto",
+        height: height || "auto",
+        padding: padding || "0",
+    };
+
+    return (
+        <div
+            className={classes}
+            style={columnsStyles}
+        >
+            {children}
+        </div>
+    )
+}
+
+
+export { Rows, Columns }
