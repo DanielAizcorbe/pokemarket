fc4876ea4efd6a888f6008c701e2f8e044b7e52a
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-02-27 13:24:18 -0300

    pequeños cambios de estilo y formato

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import { Body } from './components/main/body/Body';
 import { Header } from './components/main/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header>
-        
+
       </Header>
+      <Body>
+      
+      </Body>
     </div>
   );
 }
