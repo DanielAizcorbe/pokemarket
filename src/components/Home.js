839ad2a1773a314d706238a9a159b7959c662764
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-03-14 03:28:08 -0300

    ajustes de tamaño de containers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./home.css";
 import { Header } from './header/Header';
+import { Ofertas } from './ofertas/Ofertas';
 
 const Home = () => {
     return (
         <div className='body'>
             <Header />
-            
+            <main>
+                <Ofertas />
+            </main>
         </div>
     )
 }
